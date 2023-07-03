c0c93a056decc9bfcebaefb58443b2d4f314ad3d
Ramkumardehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2023-07-03 07:48:39 +0530

    Time: 499 ms (96.33%), Space: 172.7 MB (77.90%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -0,0 +1,47 @@
+template<typename T>
+struct FenwickTree{
+   vector<T> bit;
+   int n;
+   void construct(vector<T> const &a){
+      this->n = a.size();
+      bit.assign(n+1,0);
+      for(int i=1; i<=n; ++i)
+         update(i,a[i-1]);
+   }
+   T sum(int r) {
+      T ret = 0;
+      for(; r>0; r-=r&-r)
+         ret += bit[r];
+      return ret;
+   }
+   T sum(int l, int r) {
+      return sum(r)-sum(l - 1);
+   }
+   void update(int idx,T delta) {
+      for(; idx<=n; idx+=idx&-idx)
+         bit[idx] += delta;
+   }
+   void range_update(int l,int r,T delta){
+      update(l,delta);
+      update(r+1,-delta);
+   }
+};
+class NumArray {
+private:
+    FenwickTree<int> ft;
+    vector<int> v;
+public:
+    NumArray(vector<int>& nums) {
+        v = nums;
+        ft.construct(nums);
+    }
+    
+    void update(int index, int val) {
+        ft.update(index+1,val-v[index]);
+        v[index] = val;
+    }
+    
+    int sumRange(int left, int right) {
+        return ft.sum(left+1,right+1);
+    }
+};
