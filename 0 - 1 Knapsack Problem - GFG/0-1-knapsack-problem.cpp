5c2fc8b7a76e3b0aa6dc758286a0479d597cfa5b
Ramkumardehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2023-07-24 22:21:46 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -24,12 +24,43 @@ class Solution
             return dp[n][W] = solve(W, wt, val, n-1, dp);
         }
     }
+    
+    int solveTab(int W, int wt[], int val[], int n){
+        
+        vector<vector<int>> dp(n+1, vector<int> (W+1, 0));
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<W; j++){
+                if(i == 0 || j== 0){
+                    dp[i][j]  = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i<n+1; i++){
+            for(int j = 1; j<W+1; j++){
+                
+                if(j >= wt[i-1]){
+                    dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+            }
+        }
+        
+        
+        return dp[n][W];
+    }
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       vector<vector<int>> dp(n+1, vector<int> (W+1, -1));
-       
-       return solve(W, wt, val, n, dp);
+    //   vector<vector<int>> dp(n+1, vector<int> (W+1, -1));
+    //   return solve(W, wt, val, n, dp);
+    
+    //Tabulation
+    return solveTab(W, wt, val, n);
     }
 };
 
