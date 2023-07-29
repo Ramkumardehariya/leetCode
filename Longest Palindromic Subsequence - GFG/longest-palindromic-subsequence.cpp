e78edc1020ebff026b0176cab69474a09f33b1eb
Ramkumardehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2023-07-29 12:06:25 +0530

    Added solution - LeetHub

---
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    
+    int solve(int n, string s1, string s2){
+		    vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+		    
+		    for(int i = 1; i<n+1; i++){
+		        for(int j = 1; j<n+1; j++){
+		            if(s1[i-1] == s2[j-1]){
+		                dp[i][j] = 1+dp[i-1][j-1];
+		            }
+		            else{
+		                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+		            }
+		        }
+		    }
+		    
+		    return dp[n][n];
+		}
+		
+    int longestPalinSubseq(string A) {
+        int n = A.size();
+        string s1 = A;
+        
+        reverse(A.begin(),A.end());
+        
+        string s2 = A;
+        
+        return solve(n,s1, s2);
+    }
+};
+
+//{ Driver Code Starts.
+
+int32_t main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout << ob.longestPalinSubseq(s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
