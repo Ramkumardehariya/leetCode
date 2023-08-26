5cca0534b625d782f03d3eca7d25a4b50c5bb159
Ramkumardehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2023-08-26 23:02:40 +0530

    Time: 11 ms (65.95%), Space: 8.4 MB (53.77%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char, int> mp;
+        // unordered_map<char, int> mp;
+        // for(auto i : s){
+        //     mp[i]++;
+        // }
+        // vector<pair<int,char>> temp;
+        // for(auto it : mp){
+        //     temp.push_back({it.second, it.first});
+        // }
+        // sort(temp.begin(), temp.end(), greater<pair<int,char>>());
+        // string ans;
 
-        for(auto i : s){
-            mp[i]++;
+        // for(auto it : temp){
+        //     for(int i= 0; i<it.first; i++){
+        //         ans.push_back(it.second);
+        //     }
+        // }
+        // return ans;
+
+        unordered_map<char,int> mp;
+        for(auto it: s){
+            mp[it]++;
+        }
+        priority_queue<pair<int,char>> pq;
+        for(auto it: mp){
+            pq.push({it.second, it.first});
         }
 
-        vector<pair<int,char>> temp;
-
-        for(auto it : mp){
-            temp.push_back({it.second, it.first});
-        }
-
-        sort(temp.begin(), temp.end(), greater<pair<int,char>>());
-
-        string ans;
-
-        for(auto it : temp){
-            for(int i= 0; i<it.first; i++){
-                ans.push_back(it.second);
+        string ans = "";
+        while(!pq.empty()){
+            int freq = pq.top().first;
+            while(freq){
+                ans += pq.top().second;
+                freq--;
             }
+            pq.pop();
         }
+
         return ans;
     }
 };
