f2d0ca0934236856c6bbac92a396180bd0ce209a
Ramkumardehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2023-09-29 09:01:02 +0530

    Time: 91 ms (34.79%), Space: 44.4 MB (16.19%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int> mp;
+        for(int i = 0; i<tasks.size(); i++){
+            mp[tasks[i]]++;
+        }
+        priority_queue<int> pq;
+        for(auto it : mp){
+            pq.push(it.second);
+        }
+
+        int result = 0;
+
+        while(!pq.empty()){
+            int time = 0;
+            vector<int> temp;
+            for(int i = 0; i<n+1; ++i){
+                if(!pq.empty()){
+                    temp.push_back(pq.top()-1);
+                    pq.pop();
+                    time++;
+                }
+            }
+
+            for(auto it : temp){
+                if(it){
+                    pq.push(it);
+                }
+            }
+            result += pq.empty() ? time : n+1;
+        }
+        return result;
+    }
+};
