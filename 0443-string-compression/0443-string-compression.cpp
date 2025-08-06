6a17c98a18ae91298f16bf820a531d0e54a5d222
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-08-06 22:48:24 +0530

    Time: 0 ms (100%), Space: 13.5 MB (92.19%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int count = 1;
-        int ans = 0;
-        vector<char> temp;
+        int count = 0;
+        int i = 0;
+        int ansIndex = 0;
+        int n = chars.size();
 
-        for(int i = 1; i<chars.size(); i++){
-            if(chars[i-1] == chars[i]){
-                count++;
+        while(i < n){
+            int j = i+1;
+
+            while(j < n && chars[i] == chars[j]){
+                j++;
             }
-            else{
-                if(count > 1 ){
-                    temp.push_back(chars[i-1]);
-                    vector<char> temp2;
-                    while(count != 0){
-                        int rem = count%10;
-                        count = count/10;
-                        temp2.push_back(rem+'0');
-                    }
-                    reverse(temp2.begin(), temp2.end());
-                    for(int k = 0; k<temp2.size(); k++){
-                        temp.push_back(temp2[k]);
-                    }
+            count = j-i;
+            chars[ansIndex++] = chars[i];
+
+            if(count > 1){
+                string cnt = to_string(count);
+                for(auto ch : cnt){
+                    chars[ansIndex++] = ch;
                 }
-                else{
-                    temp.push_back(chars[i-1]);
-                }
-                count = 1;
             }
+            i = j;
         }
-        if(count > 1 ){
-                    temp.push_back(chars[chars.size()-1]);
-                    vector<char> temp2;
-                    while(count != 0){
-                        int rem = count%10;
-                        count = count/10;
-                        temp2.push_back(rem+'0');
-                    }
-                    reverse(temp2.begin(), temp2.end());
-                    for(int k = 0; k<temp2.size(); k++){
-                        temp.push_back(temp2[k]);
-                    }
-                }
-        else{
-            temp.push_back(chars[chars.size()-1]);
-        }
-                chars = temp;
-        return temp.size();
+        return ansIndex;
     }
 };
