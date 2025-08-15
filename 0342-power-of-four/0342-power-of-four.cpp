c161e37b9b9f4b5c75218fa14cc5ba5340ed902b
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-08-15 14:01:01 +0530

    Time: 0 ms (100%), Space: 7.9 MB (52.41%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
 
-        // if(n == 1){
-        //     return 1;
-        // }
-        // if(n<=0){
-        //     return 0;
-        // }
-        // return n%4 == 0 && isPowerOfFour(n/4);
-
-
-        if(n <= 0){
+    bool solve(int n){
+        if(n == 1){
+            return true;
+        }
+        if(n%4 != 0 || n == 0){
             return false;
         }
-
-        double logval = log10(n)/log10(4);
-        return (logval-int(logval)) == 0;
+        return solve(n/4);
+    }
+    bool isPowerOfFour(int n) {
+        return solve(n);
     }
 };
