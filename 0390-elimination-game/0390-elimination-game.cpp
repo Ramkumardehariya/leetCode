0737d60b1876f03a93a3bac0b8dc2de05a101f2e
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-09-13 11:48:15 +0530

    Time: 3 ms (13.54%), Space: 9.7 MB (45.55%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-
-        int remain = n;
-        bool flag = true;
-        int head = 1;
-        int step = 1;
-
-        while(remain > 1){
-            if(flag || remain % 2 == 1){
-                head += step;
-            }
-
-            remain /= 2;
-            step *= 2;
-            flag = !flag;
+        if(n == 1){
+            return n;
         }
-        return head;
 
+        return 2*(1 + (n/2) - lastRemaining(n/2));
     }
 };
