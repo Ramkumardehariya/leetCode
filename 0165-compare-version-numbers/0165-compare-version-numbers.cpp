179cedeaf73c7da7237f0e5a1d31197c3394ecda
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-09-23 12:52:54 +0530

    Time: 0 ms (100%), Space: 8.3 MB (69.84%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,56 +1,70 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        vector<int> v1;
-        vector<int> v2;
-
-        long long ans = 0;
+        long long num1 = 0;
+        queue<long long> q1;
+        queue<long long> q2;
 
         for(int i = 0; i<version1.size(); i++){
-            if(version1[i] == '.'){
-                v1.push_back(ans);
-                ans = 0;
+            if(version1[i] != '.'){
+                num1 = num1*10+version1[i]-'0';
             }
             else{
-                ans = ans * 10 + (version1[i]-'0');
+                q1.push(num1);
+                num1 = 0;
             }
         }
-        v1.push_back(ans);
 
-        ans = 0;
+        if(num1 != 0){
+            q1.push(num1);
+        }
+        num1 = 0;
 
         for(int i = 0; i<version2.size(); i++){
-            if(version2[i] == '.'){
-                v2.push_back(ans);
-                ans = 0;
+            if(version2[i] != '.'){
+                num1 = num1*10+version2[i]-'0';
             }
             else{
-                ans = ans * 10 + (version2[i]-'0');
+                q2.push(num1);
+                num1 = 0;
             }
         }
 
-        v2.push_back(ans);
-
-        if(v1.size() < v2.size()){
-
-            while(v1.size() != v2.size()){
-                v1.push_back(0);
-            }
-        }
-        else{
-            while(v1.size() != v2.size()){
-                v2.push_back(0);
-            }
+        if(num1 != 0){
+            q2.push(num1);
         }
 
-        for(int i = 0; i<v1.size(); i++){
-            if(v1[i] < v2[i]){
-                return -1;
-            }
-            if(v1[i] > v2[i]){
+        while(!q1.empty() && !q2.empty()){
+            long long n1 = q1.front();
+            long long n2 = q2.front();
+            q1.pop();
+            q2.pop();
+
+            if(n1 > n2){
                 return 1;
             }
+            else if(n1 < n2){
+                return -1;
+            }
         }
+
+        
+        while(!q1.empty() && q1.front() == 0){
+            q1.pop();
+        }
+
+        while(!q2.empty() && q2.front() == 0){
+            q2.pop();
+        }
+
+
+        if(!q1.empty() && q2.empty()){
+            return 1;
+        }
+        else if(q1.empty() && !q2.empty()){
+            return -1;
+        }
+        
 
         return 0;
     }
