46eb9a78ca1dc0ea36ad7690c2cf68b7c2d3e3a6
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-10-15 09:57:09 +0530

    Time: 1 ms (63.01%), Space: 11.8 MB (24.24%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    string ones[20] = {""," One", " Two", " Three", " Four", " Five", " Six", " Seven", " Eight", " Nine", " Ten", " Eleven",
-                        " Twelve", " Thirteen", " Fourteen", " Fifteen", " Sixteen"," Seventeen", " Eighteen", " Nineteen"};
-    string tens[10] = {"", " Ten"," Twenty", " Thirty", " Forty", " Fifty", " Sixty"," Seventy"," Eighty"," Ninety"};
-    string thousand[4] = {"", " Thousand", " Million", " Billion"};
-    
-    
-    string solve(int num){
-        if(num < 20){
-            return ones[num];
-        }
-        if(num < 100){
-            return tens[num/10] + solve(num%10);
-        }
-        if(num < 1000){
-            return solve(num/100) +" Hundred"+ solve(num%100);
+    vector<pair<int, string>> arr = {{1000000000, "Billion "}, {1000000, "Million "}, {1000, "Thousand "}, 
+    {100, "Hundred "}, {90, "Ninety "}, {80, "Eighty "}, {70, "Seventy "}, {60, "Sixty "}, {50, "Fifty "}, 
+    {40, "Forty "}, {30, "Thirty "}, {20, "Twenty "}, {19, "Nineteen "}, {18, "Eighteen "}, 
+    {17, "Seventeen "}, {16, "Sixteen "}, {15, "Fifteen "}, {14, "Fourteen "}, {13, "Thirteen "}, 
+    {12, "Twelve "}, {11, "Eleven "}, {10, "Ten "}, {9, "Nine "}, {8, "Eight "}, {7, "Seven "}, 
+    {6, "Six "}, {5, "Five "}, {4, "Four "}, {3, "Three "}, {2, "Two "}, {1, "One "}};
+
+    string solve(int n){
+        if(n == 0){
+            return "Zero ";
         }
 
-        for(int i = 3; i>=0; i--){
-            if(num >= pow(1000,i)){
-                return solve(num/pow(1000,i)) +thousand[i]+ solve(num%(int)pow(1000,i));
+        for(auto i: arr){
+            if(n >= i.first){
+                return (n >= 100 ? solve(n/i.first) : "") + (i.second) + (n%i.first == 0 ? "" : solve(n%i.first));
             }
         }
+
         return "";
     }
     
     string numberToWords(int num) {
-        if(num == 0){
-            return "Zero";
-        }
-        return solve(num).substr(1);
+        string ans = solve(num);
+        ans.pop_back();
+        return ans;
     }
 };
