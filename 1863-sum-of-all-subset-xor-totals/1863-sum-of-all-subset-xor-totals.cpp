02cab9614b4b6e7ed13f2f73b2217ca3b5103550
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-10-17 16:54:51 +0530

    Time: 0 ms (100%), Space: 9.3 MB (20.55%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
+    void solve(int i, vector<int>&nums,int &ans, int xorAns){
+        if(i == nums.size()){
+            ans += xorAns;
+            return;
+        }
+
+        xorAns =xorAns^nums[i];
+        solve(i+1, nums, ans, xorAns);
+        xorAns = xorAns^nums[i];
+        solve(i+1, nums, ans, xorAns);
+    }
     int subsetXORSum(vector<int>& nums) {
         int ans = 0;
-        vector<vector<int>> subset;
-        int n = nums.size();
-
-        for(int i = 0; i < (1 << n); i++) {
-            vector<int> temp;
-            for(int j = 0; j < n; j++) {
-                if((i & (1 << j)) != 0) {
-                    temp.push_back(nums[j]);
-                }
-            }
-            subset.push_back(temp);
-        }
-
-        for(int i = 0; i<subset.size(); i++){
-            int sum = 0;
-            for(int j = 0; j<subset[i].size(); j++){
-                sum ^= subset[i][j];
-            }
-            ans += sum;
-        }
-
+        solve(0, nums, ans, 0);
         return ans;
     }
 };
