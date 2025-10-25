8bd24f825dfd83a712bff864ec56dcfad2645989
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-10-25 10:46:20 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.15%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -3,15 +3,15 @@ public:
     int totalMoney(int n) {
         int ans = 0;
         int count = 1;
-        int check = 1;
-
+        int week = 1;
         for(int i = 1; i<=n; i++){
-            ans += count;
-
             if(i%7 == 0){
-                count = check;
-                check++;
+                week++;
+                ans += count;
+                count = week;
+                continue;
             }
+            ans += count;
             count++;
         }
         return ans;
