afdb0f5136b6b01364d09d570aae72abd492a6ba
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-10-27 13:07:39 +0530

    Time: 3 ms (65.69%), Space: 8.3 MB (11.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -7,7 +7,21 @@ public:
 
         return solve(n-1)+solve(n-2);
     }
+    
+    int solveMem(int n, vector<int> &dp){
+        if(n <= 1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = solveMem(n-1,dp)+solveMem(n-2, dp);
+    }
+    
     int fib(int n) {
-        return solve(n);
+        //int ans = solve(n);
+        vector<int> dp(n+1, -1);
+        int ans = solveMem(n, dp);
+        return ans;
     }
 };
