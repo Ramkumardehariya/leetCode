a75ca3ef62fb2a502cdc47a6b5017bc905127a83
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-11-14 19:52:14 +0530

    Time: 23 ms (19.4%), Space: 30.9 MB (78.55%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,90 +1,39 @@
 class Solution {
 public:
 
-    int solve(vector<vector<char>>& matrix, int i, int j, int &maxi){
-        if(i>=matrix.size() || j >= matrix[0].size()){
-            return 0;
-        }
-
-        int num = 0;
-        int right = solve(matrix, i, j+1, maxi);
-        int diagonal = solve(matrix, i+1, j+1, maxi);
-        int down = solve(matrix, i+1, j, maxi);
-
-        if(matrix[i][j] == '1'){
-            int ans = 1 + min(right, min(diagonal, down));
-            maxi = max(maxi, ans);
-            return ans;
-        }
-        else{
-            return 0;
-        }
-    }
-
-    int solveMem(vector<vector<char>>& matrix, int i, int j, int &maxi, vector<vector<int>>&dp){
-        if(i >= matrix.size() || j >= matrix[0].size()){
+    int solve(int i, int j, vector<vector<char>> &matrix, int &maxi, vector<vector<int>> &dp){
+        if(i >= matrix.size() || j>= matrix[0].size()){
             return 0;
         }
 
         if(dp[i][j] != -1){
             return dp[i][j];
         }
+        int bottom = solve(i+1, j, matrix, maxi, dp);
+        int diagonal = solve(i+1, j+1, matrix, maxi, dp);
+        int right = solve(i, j+1, matrix, maxi, dp);
 
-        int right = solveMem(matrix, i, j+1, maxi, dp);
-        int diagonal = solveMem(matrix, i+1, j+1, maxi, dp);
-        int down = solveMem(matrix, i+1, j, maxi, dp);
 
         if(matrix[i][j] == '1'){
-            dp[i][j] = 1 + min(right, min(diagonal, down));
-            maxi = max(maxi, dp[i][j]);
-
-            return dp[i][j];
-
+             int side = min(bottom , min(diagonal, right))+1;
+             maxi = max(maxi, side);
+             return dp[i][j] = side;
         }
         else{
             return dp[i][j] = 0;
         }
     }
 
-    int solveTab(vector<vector<char>>& matrix, int &maxi){
-
-        int row = matrix.size();
-        int col = matrix[0].size();
-
-        vector<vector<int>> dp(row+1, vector<int>(col+1, 0));
-
-        for(int i = row-1; i>=0; i--){
-            for(int j = col-1; j>=0; j--){
-                int right = dp[i][j+1];
-                int diagonal = dp[i+1][j+1];
-                int down = dp[i+1][j];
-
-                if(matrix[i][j] == '1'){
-                    dp[i][j] = 1 + min(right, min(diagonal, down));
-                    maxi = max(dp[i][j],maxi);
-                }
-                else{
-                    dp[i][j] = 0;
-                }
-            }
-        }
-        return dp[0][0];
-    }
 
     int maximalSquare(vector<vector<char>>& matrix) {
         int maxi = 0;
-        //recursion
-        // solve(matrix, 0, 0, maxi);
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        //memoization
-        // int n = matrix.size();
-        // int m = matrix[0].size();
-        // vector<vector<int> >dp(n,vector<int> (m,-1));
-        // solveMem(matrix,0,0,maxi,dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        solve(0, 0, matrix, maxi, dp);
 
 
-        //solve tabulization method
-        solveTab(matrix,maxi);
         return maxi*maxi;
     }
 };
