be20804df74e4e3113fc3c585f300f6c160de379
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-11-20 13:30:31 +0530

    Time: 3 ms (91.52%), Space: 15.8 MB (13.17%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -25,8 +25,36 @@ public:
 
         return dp[i][j] = ans;
     }
+
+    int solveTab(string &a, string &b){
+        vector<vector<int>> dp(a.size()+1, vector<int>(b.size()+1, 0));
+
+        for(int j = 0; j<b.size(); j++){
+            dp[a.size()][j] = b.size()-j;
+        }
+        for(int i = 0; i<a.size(); i++){
+            dp[i][b.size()] = a.size()-i;
+        }
+
+        for(int i = a.size()-1; i>=0; i--){
+            for(int j = b.size()-1; j>=0; j--){
+                int ans = 0;
+                if(a[i] == b[j]){
+                    ans = dp[i+1][j+1];
+                }
+                else{
+                    ans = 1+min(dp[i][j+1], min(dp[i+1][j+1], dp[i+1][j]));
+                }
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[0][0];
+    }
     int minDistance(string word1, string word2) {
         vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
-        return solve(word1, word2, 0, 0, dp);
+        //return solve(word1, word2, 0, 0, dp);
+
+        return solveTab(word1, word2);
     }
 };
