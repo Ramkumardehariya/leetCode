4e9dfaca1f60b3a5e7ed89786b7fd482973180a2
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2025-12-07 15:34:32 +0530

    Time: 82 ms (36.14%), Space: 11 MB (79.43%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int solve(int start, int end){
+    int solve(int start, int end, vector<vector<int>> &dp){
         if(start >= end){
             return 0;
         }
+
+        if(dp[start][end] != -1){
+            return dp[start][end];
+        }
         int mini = INT_MAX;
         for(int i = start; i<=end; i++){
-            mini = min(mini, i+max(solve(start, i-1), solve(i+1, end)));
+            mini = min(mini, i+max(solve(start, i-1, dp), solve(i+1, end, dp)));
         }
-        return mini;
+        return dp[start][end] = mini;
     }
     int getMoneyAmount(int n) {
-        return solve(1,n);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(1,n, dp);
     }
 };
