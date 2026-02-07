75be318245d989b136d30fd25b82a6bfb3ab818a
Ramkumar dehariya <114914262+Ramkumardehariya@users.noreply.github.com>
2026-02-07 17:47:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        int arr1[26] = {0};
-        int arr2[26] = {0};
 
-        if(s1.size() > s2.size()){
+    bool isSame(int *first, int *second){
+
+        for(int i = 0; i<26; i++){
+            if(first[i] != second[i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        if(s2.size() < s1.size()){
             return false;
         }
+        int first[26] = {0};
+        int second[26] = {0};
 
-        for(auto ch: s1){
-            arr1[ch-'a']++;
-        }
         for(int i = 0; i<s1.size(); i++){
-            arr2[s2[i]-'a']++;
-        }
-
-        if(arr1 == arr2){
-            return true;
+            first[s1[i]-'a']++;
+            second[s2[i]-'a']++;
         }
 
         int i = 0;
-        int j = s1.size();
-        int n = s2.size();
 
-        while(j < n){
-
-            arr2[s2[i]-'a']--;
-            arr2[s2[j]-'a']++;
-
-            if(arr1 == arr2){
+        for(int j = s1.size(); j<s2.size(); j++){
+            if(isSame(first, second)){
                 return true;
             }
-            
-            bool flag = true;
-            for(int i = 0;i<26; i++){
-                if(arr1[i] != arr2[i]){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag){
-                return true;
-            }
+            second[s2[i]-'a']--;
+            second[s2[j]-'a']++;
             i++;
-            j++;
         }
 
         return false;
